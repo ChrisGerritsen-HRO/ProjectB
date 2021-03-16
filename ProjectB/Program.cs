0305aca536b9842bdfa4dffc76213f52ded1a4f9
cgerritsen <cgerritsen@flaire.nl>
2021-03-16 13:41:56 +0100

    dit is een test

--- ProjectB/Program.cs
+++ ProjectB/Program.cs
@@ -7,6 +7,7 @@ namespace ProjectB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Deze branch is van chris");
         }
     }
 }
