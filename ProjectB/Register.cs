0fdc30edaf783e254ab9ce96eff638cf82661f40
Chris <christiaangerritsen2000@gmail.com>
2021-03-22 14:14:06 +0100

    Added new user input check

--- ProjectB/Register.cs
+++ ProjectB/Register.cs
@@ -14,22 +14,43 @@ namespace ProjectB
         public static void userInput() {
             var dateFormats = new[] {"dd.MM.yyyy", "dd-MM-yyyy", "dd/MM/yyyy"};
             DateTime dateValue;
+            
+            while(true) {
+                Console.WriteLine("Vul uw voornaam in: ");
+                string firstName = Console.ReadLine();
 
-            Console.WriteLine("Vul uw voornaam in: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Vul uw achternaam in: ");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Vul uw email in: ");
-            string userEmail = Console.ReadLine();
-            Console.WriteLine("Vul uw geboortedatum in: ");
-            string birthDay = Console.ReadLine();
+                if(Regex.IsMatch(firstName, @"^[a-zA-Z]+$")) { break; }
+                else { Console.WriteLine("Er klopt iets niet aan uw naam."); }
+            }
+            while(true) {
+                Console.WriteLine("Vul uw achternaam in: ");
+                string lastName = Console.ReadLine();
 
-            if(Regex.IsMatch(firstName, @"^[a-zA-Z]+$") && Regex.IsMatch(lastName, @"^[a-zA-Z]+$")
-             && userEmail.Contains("@") && DateTime.TryParse(birthDay, out dateValue)) {
-                Console.WriteLine("Bedankt!");
-            } else {
-                Console.WriteLine("Er klopt iets niet aan de gegevens.");
-                userInput();
+                if(Regex.IsMatch(lastName, @"^[a-zA-Z]+$")) { break; }
+                else { Console.WriteLine("Er klopt iets niet aan uw naam."); }
+            }
+            while(true) {
+                Console.WriteLine("Vul uw email in: ");
+                string userEmail = Console.ReadLine();
+
+                if(userEmail.Contains("@")) { break; }
+                else { Console.WriteLine("Er klopt iets niet aan uw email."); }
+            }
+            while(true) {
+                Console.WriteLine("Vul uw geboortedatum in: ");
+                string birthDay = Console.ReadLine();
+
+                if(DateTime.TryParse(birthDay, out dateValue)) { break; }
+                else { Console.WriteLine("Er klopt iets niet aan uw geboortedatum."); }
+            }
+            while(true) {
+                Console.WriteLine("Vul een wachtwoord in: ");
+                string password = Console.ReadLine();
+                Console.WriteLine("Herhaal wachtwoord: ");
+                string passwordConfirm = Console.ReadLine();
+
+                if(password == passwordConfirm) { break; }
+                else { Console.WriteLine("De wachtwoorden komen niet overeen."); }
             }
         }
     }
