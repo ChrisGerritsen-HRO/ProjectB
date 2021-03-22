3e638f551693c7edac5a9d36178e5352f55c2ea3
cgerritsen <cgerritsen@flaire.nl>
2021-03-22 13:32:14 +0100

    Added date check

--- ProjectB/Register.cs
+++ ProjectB/Register.cs
@@ -12,14 +12,20 @@ namespace ProjectB
         }
 
         public static void userInput() {
+            var dateFormats = new[] {"dd.MM.yyyy", "dd-MM-yyyy", "dd/MM/yyyy"};
+            DateTime dateValue;
+
             Console.WriteLine("Vul uw voornaam in: ");
             string firstName = Console.ReadLine();
             Console.WriteLine("Vul uw achternaam in: ");
             string lastName = Console.ReadLine();
-            Console.WriteLine("Vul uw email: ");
+            Console.WriteLine("Vul uw email in: ");
             string userEmail = Console.ReadLine();
+            Console.WriteLine("Vul uw geboortedatum in: ");
+            string birthDay = Console.ReadLine();
 
-            if(Regex.IsMatch(firstName, @"^[a-zA-Z]+$") && Regex.IsMatch(lastName, @"^[a-zA-Z]+$") && userEmail.Contains("@")) {
+            if(Regex.IsMatch(firstName, @"^[a-zA-Z]+$") && Regex.IsMatch(lastName, @"^[a-zA-Z]+$")
+             && userEmail.Contains("@") && DateTime.TryParse(birthDay, out dateValue)) {
                 Console.WriteLine("Bedankt!");
             } else {
                 Console.WriteLine("Er klopt iets niet aan de gegevens.");
