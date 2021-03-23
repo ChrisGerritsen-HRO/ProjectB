31ccca2c26ea5e104b0c4dba4de775014c29e0ac
Chris <christiaangerritsen2000@gmail.com>
2021-03-23 13:58:40 +0100

    Addes new input fields

--- ProjectB/Register.cs
+++ ProjectB/Register.cs
@@ -7,9 +7,11 @@ namespace ProjectB
 {
     public class Account {
         public string userEmail { get; set; }
+        public string insertion { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string birthDay { get; set; } 
+        public string gender { get; set; }
         public string password { get; set; }
         public string role { get; set; }
     }
@@ -21,16 +23,23 @@ namespace ProjectB
         }
 
         public static void userInput() {
-            string firstName, lastName, userEmail, birthDay, password;
+            string firstName, lastName, insertion, userEmail, birthDay, password, gender;
             var dateFormats = new[] {"dd.MM.yyyy", "dd-MM-yyyy", "dd/MM/yyyy"};
             DateTime dateValue;
-            
+
             while(true) {
                 Console.WriteLine("Vul uw voornaam in: ");
                 firstName = Console.ReadLine();
 
                 if(Regex.IsMatch(firstName, @"^[a-zA-Z]+$")) { break; }
                 else { Console.WriteLine("Er klopt iets niet aan uw naam."); }
+            }
+            while(true) {
+                Console.WriteLine("[Optioneel] Vul uw tussenvoegsel in: ");
+                insertion = Console.ReadLine();
+
+                if(Regex.IsMatch(insertion, @"(?i)^[a-z.,\s]+$") || insertion == "") { break; }
+                else { Console.WriteLine("Er klopt iets niet aan uw tussenvoegsel."); }
             }
             while(true) {
                 Console.WriteLine("Vul uw achternaam in: ");
@@ -42,8 +51,10 @@ namespace ProjectB
             while(true) {
                 Console.WriteLine("Vul uw email in: ");
                 userEmail = Console.ReadLine();
+                Console.WriteLine("Herhaal uw email: ");
+                string userEmailConfirm = Console.ReadLine();
 
-                if(userEmail.Contains("@")) { break; }
+                if(Regex.IsMatch(userEmail, "^[A-Za-z0-9_.-]{1,64}@[A-Za-z-]{1,255}.(com|net|nl|org)$") && userEmail == userEmailConfirm) { break; }
                 else { Console.WriteLine("Er klopt iets niet aan uw email."); }
             }
             while(true) {
@@ -54,12 +65,22 @@ namespace ProjectB
                 else { Console.WriteLine("Er klopt iets niet aan uw geboortedatum."); }
             }
             while(true) {
+                Console.WriteLine("Wat is uw geslacht? ");
+                Console.WriteLine("[1] Man \n [2] Vrouw \n [3] Onbekend");
+                gender = Console.ReadLine();
+
+                if(gender == "1") { gender = "man"; break; }
+                else if(gender == "2") { gender = "vrouw"; break; }
+                else if(gender == "3") { gender = "Onbekend"; break; }
+                else { Console.WriteLine("Uw heeft niet een van de bovenstaande keuzes gekozen."); }
+            }
+            while(true) {
                 Console.WriteLine("Vul een wachtwoord in: ");
                 password = Console.ReadLine();
                 Console.WriteLine("Herhaal wachtwoord: ");
                 string passwordConfirm = Console.ReadLine();
 
-                if(password == passwordConfirm) { break; }
+                if(password == passwordConfirm && password.Length >= 6) { break; }
                 else { Console.WriteLine("De wachtwoorden komen niet overeen."); }
             }
 
@@ -67,8 +88,10 @@ namespace ProjectB
             var obj = new Account {
                 userEmail = userEmail,
                 firstName = firstName,
+                insertion = insertion,
                 lastName = lastName,
                 birthDay = birthDay,
+                gender = gender,
                 password = password,
                 role = "user"
             };
