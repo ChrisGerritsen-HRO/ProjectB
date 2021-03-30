2226d343d843be3584f18023a9fbddb10b4dd358
StefanNijssen435 <80760925+StefanNijssen435@users.noreply.github.com>
2021-03-30 11:00:28 +0200

    Add files via upload
    begin aan het zoeken in films

--- ProjectB/Program.cs
+++ ProjectB/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 namespace ProjectB
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class Movies {
+
+
+
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
+
+
+            Console.WriteLine("Voer een tijd of naam van een film in:");
+            string strInput = Console.ReadLine();
+            
+
+            if (Regex.IsMatch(strInput, "[a-zA-Z]"))
+            {
+                Console.WriteLine("Deze films zijn beschikbaar voor de zoekterm: " + strInput);
+            }
+
+             else if (double.TryParse(strInput, out double result))
+
+
+             {
+                 Console.WriteLine("Deze films zijn beschikbaar om {0} uur:", result);
+
+             }
+             else
+             {
+                 Console.WriteLine("Er zijn geen beschikbare films voor deze tijd of zoekterm " + strInput);
+             }
+
+            }
+        }      
     }
-}
+     
