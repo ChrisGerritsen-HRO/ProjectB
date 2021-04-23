69bfe4c76d303f6832d2a2c98f6a13644f678a4f
Sawanthi <1013338@hr.nl>
2021-04-23 18:08:16 +0200

    flashed nietmeer, thank god

--- ProjectB/menu.cs
+++ ProjectB/menu.cs
@@ -12,13 +12,13 @@ namespace ConsoleApp1
             Console.ResetColor();
         }
 
-        static string Menu(string[] menu, int menuColor)
+        /*static string Menu(string[] menu, int menuColor)
         {
             int currentItem = 0;
             while (true)
             {
-                Console.Clear();
-                for(int i = 0; i < menu.Length; i++)
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < menu.Length; i++)
                 {
                     if (currentItem == i)
                     {
@@ -52,10 +52,49 @@ namespace ConsoleApp1
             }
             Console.Clear();
             return menu[currentItem];
+        }*/
+        static string Menu(string title, string[] items, int titleColor, int SelectColor)
+        {
+            int currentItem = 0; //geselecteerde item
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                textColor(title, titleColor, false); //print title
+                for (int i = 0; i < items.Length; i++) //print alle items uit het menu, met de geselecteerde item gekleurd
+                {
+                    if (currentItem == i)
+                    {
+                        textColor(">>" + items[i] + "  ", SelectColor, false);
+                    }
+                    else
+                    {
+                        textColor("  " + items[i] + "  ", 15, false);
+                    }
+                }
+                ConsoleKeyInfo key;
+                key = Console.ReadKey(true); //wacht tot de user een input geeft
+                if (key.Key.ToString() == "DownArrow")
+                {
+                    currentItem++;
+                    if (currentItem > items.Length - 1) { currentItem = 0; }
+                }
+                else if (key.Key.ToString() == "UpArrow")
+                {
+                    currentItem--;
+                    if (currentItem < 0) { currentItem = items.Length - 1; }
+                }
+                else if (key.Key.ToString() == "Enter")
+                {
+                    break;
+                }
+                else { continue; }
+            }
+            Console.Clear();
+            return items[currentItem]; //geeft de naam van de item dat geselecteerd is terug 
         }
         public static void Main()
         {
-            Console.WriteLine(Menu(new string[] { "this", "is", "a", "test" }, 11));
+            Console.WriteLine(Menu("Test", new string[] { "this", "is", "a", "test" }, 11, 9));
         }
     }
 }
