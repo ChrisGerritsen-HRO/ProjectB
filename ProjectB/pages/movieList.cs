1269d1d3d522070847044d8361df3e430756de57
StefanNijssen <58977715+StefanNijssen@users.noreply.github.com>
2021-04-28 12:54:49 +0200

    kleine aanpassingen

--- ProjectB/pages/movieList.cs
+++ ProjectB/pages/movieList.cs
@@ -16,7 +16,7 @@ namespace ProjectB.pages
              while(true) {
                 var userinput = Console.ReadLine();
                 if (userinput == "1") {
-                    filmlijst();
+                    listMain();
                     Menu.mainMenu();
                     break;
                     } 
@@ -26,13 +26,14 @@ namespace ProjectB.pages
                 } else {
                     tools.textColor("Alleen optie 1 en 2 zijn beschikbaar", 4, false); 
         }}}
-        public static void filmlijst() {
+        public static void listMain() {
             Console.Clear();
             string fileContent = File.ReadAllText("storage.json");
             dynamic obj = JsonConvert.DeserializeObject(fileContent);
 
             var len = ((Newtonsoft.Json.Linq.JArray)obj.movie).Count;
             for(int i = 0; i < len; i++) {
+                tools.textColor("----------------------------", 14, false);
                 tools.textColor($"Naam         | {obj.movie[i].movieName}\nBeschrijving | {obj.movie[i].movieDescription}\nLeeftijd     | {obj.movie[i].movieAge}+\nGenre     | {obj.movie[i].movieGenre}\nTijdstip     | {obj.movie[i].movieTime}\nDuur         | {obj.movie[i].movieDuration} minuten\nZaal         | {obj.movie[i].movieTheater}\n", 14, false);
             } 
 
