cf60a3f5d0e724f03379a502b365112ff72e4cb2
Sawanthi <1013338@hr.nl>
2021-05-18 11:49:11 +0200

    ja...

--- ProjectB/pages/Register.cs
+++ ProjectB/pages/Register.cs
@@ -122,6 +122,7 @@ namespace ProjectB.pages
             // JSON
             dataStorageHandler.storage.personAccount.Add(obj);
             dataStorageHandler.saveChanges();
+            Console.Clear();
             Menu.Mainmenu();
         }
     }
--- ProjectB/pages/movieList.cs
+++ ProjectB/pages/movieList.cs
@@ -23,6 +23,7 @@ namespace ProjectB.pages
         }
         public static void listMain() {
             Console.Clear();
+
             string fileContent = File.ReadAllText("storage.json");
             dynamic obj = JsonConvert.DeserializeObject(fileContent);
 
@@ -37,22 +38,5 @@ namespace ProjectB.pages
                 Menu.userMenu();
             }
         }
-
-        public static void listMainNoUser() {
-            Console.Clear();
-            string fileContent = File.ReadAllText("storage.json");
-            dynamic obj = JsonConvert.DeserializeObject(fileContent);
-
-            var len = ((Newtonsoft.Json.Linq.JArray)obj.movie).Count;
-            for(int i = 0; i < len; i++) {
-                tools.textColor("----------------------------", 14, false);
-                tools.textColor($"Naam         | {obj.movie[i].movieName}\nBeschrijving | {obj.movie[i].movieDescription}\nLeeftijd     | {obj.movie[i].movieAge}+\nGenre     | {obj.movie[i].movieGenre}\nTijdstip     | {obj.movie[i].movieTime}\nDuur         | {obj.movie[i].movieDuration} minuten\nZaal         | {obj.movie[i].movieTheater}\n", 14, false);
-            } 
-
-            string back = Menu.Menubuilder($"" + "\n", new string[] {"Terug?"}, 14, 14);
-            if(back == "Terug?") {
-                Menu.Mainmenu();
-            }
-        }    
     }
 }
